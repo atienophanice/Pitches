dc906387212e5a7152cf2e372a72076ff6ce787c
Phanice <maureezgaga@gmail.com>
2020-02-19 09:12:55 +0300

    added config.py file

---
+++ config .py
@@ -0,0 +1,33 @@
+import os
+
+
+
+class Config:
+
+    
+    SECRET_KEY = '1999'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://wecode:oppo@localhost/pitch'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    
+
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
+
+class ProdConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    pass
+
+
+class DevConfig(Config):
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
